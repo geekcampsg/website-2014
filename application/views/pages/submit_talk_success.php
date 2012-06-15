3f67252e0938920f76965bbb8d1425bd42a060e0
laurence <laurenceputra@gmail.com>
2012-06-16 00:14:06 +0800

    updated submitted talks success

--- application/views/pages/submit_talk_success.php
+++ application/views/pages/submit_talk_success.php
@@ -5,10 +5,8 @@
         <?php }  ?>
         <h2 class="about">Share your talk!</h2>
         <p class="subtitle">Let people know about your talk by sharing it/liking it below!</p>
-        <p>
-            <iframe src="//www.facebook.com/plugins/like.php?href=<?php echo urlencode(site_url('pages/index/'.date('Y').'/'.set_value('title').'/'.set_value('speaker-name').'#'.date('Y').'-'.set_value('title').'-'.set_value('speaker-name'))); ?>&amp;send=false&amp;layout=button_count&amp;width=95&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font=segoe+ui&amp;height=21&amp;appId=275526672542963" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:95px; height:21px;" allowTransparency="true"></iframe>
-            <div class="g-plusone" data-href="<?php echo site_url('pages/index/'.date('Y').'/'.set_value('title').'/'.set_value('speaker-name').'#'.date('Y').'-'.set_value('title').'-'.set_value('speaker-name'))?>" ></div>
-        </p>
+        <iframe src="//www.facebook.com/plugins/like.php?href=<?php echo urlencode(site_url('pages/index/'.date('Y').'/'.set_value('title').'/'.set_value('speaker-name').'#'.date('Y').'-'.set_value('title').'-'.set_value('speaker-name'))); ?>&amp;send=false&amp;layout=button_count&amp;width=95&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font=segoe+ui&amp;height=21&amp;appId=275526672542963" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:95px; height:21px;" allowTransparency="true"></iframe>
+        <div class="g-plusone" data-href="<?php echo site_url('pages/index/'.date('Y').'/'.set_value('title').'/'.set_value('speaker-name').'#'.date('Y').'-'.set_value('title').'-'.set_value('speaker-name'))?>" ></div>
     </div>
 </div>
 <script type="text/javascript">
