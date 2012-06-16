958960c75fadb0c347b1ae6ce65ee6d210c89bda
laurence <laurenceputra@gmail.com>
2012-06-16 11:47:34 +0800

    updated the like image

--- application/views/core.php
+++ application/views/core.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>Geekcamp.SG</title>
     <meta name="description" content="Geekcamp.SG webpage">
-
+    <meta property="og:image" content="http://geekcamp.pbworks.com/f/1314843253/geekcamp2011_website.png" />
     <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
