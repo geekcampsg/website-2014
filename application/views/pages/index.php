a31b3723d2fbf6956319dc4aa58306922bff160a
laurence <laurenceputra@gmail.com>
2012-07-04 15:34:17 +0800

    added sponsors

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -72,6 +72,10 @@
   <h2 class="about">Partners</h2>
   <p class="sublabel">Interested to work with us?</p>
   <p class="sublabel">Drop us an email <a href="<?php echo site_url('pages/email')?>">here</a>.</p>
+  <h3 class="about">Platinum Sponsors</h3>
+    <a href="http://www.windowsazure.com/" target="_blank"><img class="platinum-sponsor-images" src="<?php echo base_url('static/images/winazure-logo.png')?>" alt="Windows Azure"></a>
+  <h3 class="about">Gold Sponsors</h3>
+    <a href="http://www.softlayer.com/cloudcash" target="_blank"><img class="gold-sponsor-images" src="<?php echo base_url('static/images/softlayer-logo.png')?>" alt="Softlayer"><p>Promo Code: SNGCLOUD01</p></a>
   <h3 class="about">Media Partners</h3>
   <div class="info">
     <a href="http://e27.sg" target="_blank"><img class="media-partner-images" src="<?php echo base_url('static/images/e27-logo.jpg')?>" alt="e27"></a>
