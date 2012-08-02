2db9ab34c32c4a65556f861a7b1231a19bfd06fb
laurenceputra <laurenceputra@gmail.com>
2012-08-02 18:39:51 +0800

    updated azure url

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -71,7 +71,7 @@
   <p class="sublabel">Interested to work with us?</p>
   <p class="sublabel">Drop us an email <a href="<?php echo site_url('pages/email')?>">here</a>.</p>
   <h3 class="about">Platinum Sponsors</h3>
-    <a href="http://www.windowsazure.com/" target="_blank"><img class="platinum-sponsor-images" src="<?php echo base_url('static/images/winazure-logo.png')?>" alt="Windows Azure"></a>
+    <a href="https://www.windowsazure.com/en-us/pricing/free-trial/?WT.mc_id=AB39D46DE" target="_blank"><img class="platinum-sponsor-images" src="<?php echo base_url('static/images/winazure-logo.png')?>" alt="Windows Azure"></a>
   <h3 class="about">Gold Sponsors</h3>
     <a href="http://www.softlayer.com/cloudcash" target="_blank"><img class="gold-sponsor-images" src="<?php echo base_url('static/images/softlayer-logo.png')?>" alt="Softlayer"><p>Promo Code: SNGCLOUD01</p></a>
   <h3 class="about">Supported By</h3>
