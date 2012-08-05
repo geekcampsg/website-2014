19d962f5b6bc38611b1909b80544d25bc03733f4
laurenceputra <laurenceputra@gmail.com>
2012-08-05 20:37:29 +0800

    updated preliminary schedule

--- application/views/schedules/schedule2012.php
+++ application/views/schedules/schedule2012.php
@@ -1,5 +1,75 @@
-<div class="span12">
+<div class="span11">
     <div class="padded-element">
-        <p class="subtitle">This year's schedule is not out yet!</p>
+        <table class="table">
+        	<thead>
+        		<tr>
+        			<th>Time</th>
+        			<th>Auditorium (21st Floor)</th>
+        			<th>Hall (22nd Floor)</th>
+        		</tr>
+        	</thead>
+        	<tbody>
+        		<tr>
+        			<td>10:00 - 10:45</td>
+        			<td>Opening</td>
+        			<td></td>
+        		</tr>
+        		<tr>
+        			<td>10:15 - 11:00</td>
+        			<td>Cryptography: Why Bitcoin is safer than your bank? by U-Zyn Chua</td>
+        			<td></td>
+        		</tr>
+        		<tr>
+        			<td>11:00 - 11:45</td>
+        			<td>HTML5 - The road to multi-platform serenity by Justin Lee</td>
+        			<td></td>
+        		</tr>
+        		<tr>
+        			<td>11:45 - 12:30</td>
+        			<td>BlackMagic with Node.js and WebSockets by Bhagaban Behera</td>
+        			<td></td>
+        		</tr>
+        		<tr>
+        			<td>12:30 - 1:30</td>
+        			<td>Lunch</td>
+        			<td></td>
+        		</tr>
+        		<tr>
+        			<td>1:30 - 2:15</td>
+        			<td>Hacking and Securing iOS Apps by Subh</td>
+        			<td>Git and SVN - Making the best of both worlds by Lim Chern Jie</td>
+        		</tr>
+        		<tr>
+        			<td>2:15 - 3:00</td>
+        			<td>Hardware is not BORING by Dave Appleton</td>
+        			<td>Windows Phone 8 App Development by Chris Ismael</td>
+        		</tr>
+        		<tr>
+        			<td>3:00 - 3:45</td>
+        			<td>Learning iOS and hunting NSZombies in 3 weeks by Calvin Cheng</td>
+        			<td>Why Common Lisp is still relevant. Introduction to the language by Elias Mårtenson</td>
+        		</tr>
+        		<tr>
+        			<td>3:45 - 4:30</td>
+        			<td>Continuous Integration for PHP with Jenkins &amp Sonar by Michael Cheng</td>
+        			<td>Designing a RESTful API server for a mobile app by Mugunth</td>
+        		</tr>
+        		<tr>
+        			<td>4:30 - 5:15</td>
+        			<td>How to stay up when your server goes down (that includes AWS too) by Laurence Putra</td>
+        			<td>FPGAs - Embedded Protoyping Made Easier by Aashish Mehta</td>
+        		</tr>
+        		<tr>
+        			<td>5:15 - 6:00</td>
+        			<td>Graph databases for fun and profit by Vivek Prahlad</td>
+        			<td>What's this thing called Scala by Raymond Tay</td>
+        		</tr>
+        		<tr>
+        			<td>6:00 till late</td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        	</tbody>
+        </table>
     </div>
 </div>
