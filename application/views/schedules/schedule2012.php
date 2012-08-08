0177dabc7baf756d9ba664600ce95a9e1b6bc280
laurenceputra <laurenceputra@gmail.com>
2012-08-08 08:35:55 +0800

    updated schedule

--- application/views/schedules/schedule2012.php
+++ application/views/schedules/schedule2012.php
@@ -16,17 +16,17 @@
         		</tr>
         		<tr>
         			<td>10:15 - 11:00</td>
-        			<td>Cryptography: Why Bitcoin is safer than your bank? by U-Zyn Chua</td>
+        			<td>Cryptography: How Bitcoin is safer than your bank? by U-Zyn Chua (Confirmed)</td>
         			<td></td>
         		</tr>
         		<tr>
         			<td>11:00 - 11:45</td>
-        			<td>HTML5 - The road to multi-platform serenity by Justin Lee</td>
+        			<td>HTML5 - The road to multi-platform serenity by Justin Lee (Confirmed)</td>
         			<td></td>
         		</tr>
         		<tr>
         			<td>11:45 - 12:30</td>
-        			<td>BlackMagic with Node.js and WebSockets by Bhagaban Behera</td>
+        			<td>BlackMagic with Node.js and WebSockets by Bhagaban Behera (Confirmed)</td>
         			<td></td>
         		</tr>
         		<tr>
@@ -35,33 +35,33 @@
         		</tr>
         		<tr>
         			<td>1:30 - 2:15</td>
-        			<td>Hacking and Securing iOS Apps by Subh</td>
-        			<td>Designing a RESTful API server for a mobile app by Mugunth</td>
+        			<td>Hacking and Securing iOS Apps by Subh （Confirmed）</td>
+        			<td>Designing a RESTful API server for a mobile app by Mugunth (Confirmed)</td>
         		</tr>
         		<tr>
         			<td>2:15 - 3:00</td>
-        			<td>Hardware is not BORING by Dave Appleton</td>
+        			<td>Hardware is not BORING by Dave Appleton (Confirmed)</td>
         			<td>Azure workshop</td>
         		</tr>
         		<tr>
         			<td>3:00 - 3:45</td>
-        			<td>Learning iOS and hunting NSZombies in 3 weeks by Calvin Cheng</td>
-        			<td>Why Common Lisp is still relevant. Introduction to the language by Elias Mårtenson</td>
+        			<td>Learning iOS and hunting NSZombies in 3 weeks by Calvin Cheng (Confirmed)</td>
+        			<td>Why Common Lisp is still relevant. Introduction to the language by Elias Mårtenson (Confirmed)</td>
         		</tr>
         		<tr>
         			<td>3:45 - 4:30</td>
-        			<td>Continuous Integration for PHP with Jenkins &amp Sonar by Michael Cheng</td>
-                    <td>Git and SVN - Making the best of both worlds by Lim Chern Jie</td>
+        			<td>Continuous Integration for PHP with Jenkins &amp Sonar by Michael Cheng (Confirmed)</td>
+                    <td>Git and SVN - Making the best of both worlds by Lim Chern Jie (Confirmed)</td>
         		</tr>
         		<tr>
         			<td>4:30 - 5:15</td>
-        			<td>How to stay up when your server goes down (that includes AWS too) by Laurence Putra</td>
-        			<td>What's this thing called Scala by Raymond Tay</td>
+        			<td>How to stay up when your server goes down (that includes AWS too) by Laurence Putra (Confirmed)</td>
+        			<td>FPGAs - Embedded Protoyping Made Easier by Aashish Mehta</td>
         		</tr>
         		<tr>
         			<td>5:15 - 6:00</td>
-        			<td>Graph databases for fun and profit by Vivek Prahlad</td>
-                    <td>Windows Phone 8 App Development by Chris Ismael</td>
+        			<td>Graph databases for fun and profit by Vivek Prahlad (Confirmed)</td>
+                    <td>Youngling Special! (Confirmed)</td>
         		</tr>
         		<tr>
         			<td>6:00 till late</td>
