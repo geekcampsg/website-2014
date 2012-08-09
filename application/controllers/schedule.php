8304dc2981a0ca4445a37e130cf775fca5387e05
laurenceputra <laurenceputra@gmail.com>
2012-08-09 08:26:06 +0800

    updated schedule page

--- application/controllers/schedule.php
+++ application/controllers/schedule.php
@@ -1,7 +1,12 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Schedule extends CI_Controller {
-    public function view_schedule($year = 2012){
+    public function view_schedule($year = -1){
+        if ($year === -1) {
+            $year = 2012;
+        }
+        $data['meta_title'] = 'Schedule for '.$year;
+        $data['meta_description'] = 'GeekcampSG\'s schedule for '.$year.'. Cya at #GeekcampSG!';
         $data['content'] = $this->load->view('schedules/schedule'.$year, '', TRUE);
         $this->load->view('core', $data);
 
