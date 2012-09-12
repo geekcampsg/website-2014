d641c0c5b67e55e07c8c67f4c50cdd95d5536d22
laurenceputra <laurenceputra@gmail.com>
2012-09-12 19:11:13 +0800

    updated pretty photo to geekcampsg

--- application/views/schedules/schedule2012.php
+++ application/views/schedules/schedule2012.php
@@ -17,17 +17,17 @@
         		</tr>
         		<tr>
         			<td>10:15 - 11:00</td>
-        			<td><a class="popup" rel="prettyPhoto[popups]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/502f7b3321ffdc000202cf11?iframe=true&width=800&height=600">Cryptography: How Bitcoin is safer than your bank?</a> by <strong>U-Zyn Chua</strong> <a class="popup" rel="prettyPhoto[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/xFeqZXjSCsI?iframe=true&width=800&height=600">(Video)</a></td>
+        			<td><a rel="geekcampsg[slides]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/502f7b3321ffdc000202cf11?iframe=true&width=800&height=600">Cryptography: How Bitcoin is safer than your bank?</a> by <strong>U-Zyn Chua</strong> <a rel="geekcampsg[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/xFeqZXjSCsI?iframe=true&width=800&height=600">(Video)</a></td>
         			<td></td>
         		</tr>
         		<tr>
         			<td>11:00 - 11:45</td>
-        			<td><a class="popup" rel="prettyPhoto[popups]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/5036e814c4145e0002034486?iframe=true&width=800&height=600">HTML5 - The road to multi-platform serenity</a> by <strong>Justin Lee</strong> <a class="popup" rel="prettyPhoto[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/z2Of3BLPRIM?iframe=true&width=800&height=600">(Video)</a></td>
+        			<td><a rel="geekcampsg[slides]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/5036e814c4145e0002034486?iframe=true&width=800&height=600">HTML5 - The road to multi-platform serenity</a> by <strong>Justin Lee</strong> <a rel="geekcampsg[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/z2Of3BLPRIM?iframe=true&width=800&height=600">(Video)</a></td>
         			<td></td>
         		</tr>
         		<tr>
         			<td>11:45 - 12:30</td>
-        			<td><a class="popup" rel="prettyPhoto[popups]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://www.slideshare.net/slideshow/embed_code/14027469?iframe=true&width=800&height=600">BlackMagic with Node.js and WebSockets</a> by <strong>Bhagaban Behera</strong> <a class="popup" rel="prettyPhoto[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/kztqXNOV4vQ?iframe=true&width=800&height=600">(Video)</a></td>
+        			<td><a rel="geekcampsg[slides]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://www.slideshare.net/slideshow/embed_code/14027469?iframe=true&width=800&height=600">BlackMagic with Node.js and WebSockets</a> by <strong>Bhagaban Behera</strong> <a rel="geekcampsg[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/kztqXNOV4vQ?iframe=true&width=800&height=600">(Video)</a></td>
         			<td></td>
         		</tr>
         		<tr>
@@ -36,33 +36,33 @@
         		</tr>
         		<tr>
         			<td>1:30 - 2:15</td>
-        			<td><a class="popup" rel="prettyPhoto[popups]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/5035f894d135ef000202d152?iframe=true&width=800&height=600">Hacking and Securing iOS Apps</a> by <strong>Subh</strong> <a class="popup" rel="prettyPhoto[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/nFjYaMHVnNQ?iframe=true&width=800&height=600">(Video)</a></td>
-                    <td><a class="popup" rel="prettyPhoto[popups]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/503654aac4145e00020059e9?iframe=true&width=800&height=600">Git and SVN - Making the best of both worlds</a> by <strong>Lim Chern Jie</strong></td>
+        			<td><a rel="geekcampsg[slides]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/5035f894d135ef000202d152?iframe=true&width=800&height=600">Hacking and Securing iOS Apps</a> by <strong>Subh</strong> <a rel="geekcampsg[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/nFjYaMHVnNQ?iframe=true&width=800&height=600">(Video)</a></td>
+                    <td><a rel="geekcampsg[slides]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/503654aac4145e00020059e9?iframe=true&width=800&height=600">Git and SVN - Making the best of both worlds</a> by <strong>Lim Chern Jie</strong></td>
         			
         		</tr>
         		<tr>
         			<td>2:15 - 3:00</td>
-        			<td><a class="popup" rel="prettyPhoto[popups]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/503881361d8148000204cc08?iframe=true&width=800&height=600">Hardware is not BORING</a> by <strong>Dave Appleton</strong> <a class="popup" rel="prettyPhoto[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/Y74TQVW_yCA?iframe=true&width=800&height=600">(Video)</a></td>
+        			<td><a rel="geekcampsg[slides]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/503881361d8148000204cc08?iframe=true&width=800&height=600">Hardware is not BORING</a> by <strong>Dave Appleton</strong> <a rel="geekcampsg[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/Y74TQVW_yCA?iframe=true&width=800&height=600">(Video)</a></td>
         			<td><a href="https://www.windowsazure.com/en-us/pricing/free-trial/?WT.mc_id=AB39D46DE" target="_blank">Why you should date Windows Azure</a> by <strong>Yap Neng Ginn</strong></td>
         		</tr>
         		<tr>
         			<td>3:00 - 3:45</td>
-        			<td><a class="popup" rel="prettyPhoto[popups]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://www.slideshare.net/slideshow/embed_code/14006860?iframe=true&width=800&height=600" >Learning iOS and hunting NSZombies in 3 weeks</a> by <strong>Calvin Cheng</strong> <a class="popup" rel="prettyPhoto[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/Wh535qOUWw4?iframe=true&width=800&height=600">(Video)</a></td>
-        			<td><a class="popup" rel="prettyPhoto[popups]" title="Loading presentation from external site. Please give it a few seconds to load." href="https://docs.google.com/presentation/embed?id=1mIHAruC3nbI-CZPVjX7AuRp-bj9xjJima0l6XrHUI6Y&start=false&loop=false&delayms=3000?iframe=true&width=800&height=600">Why Common Lisp is still relevant. Introduction to the language</a> by <strong>Elias Mårtenson</strong> <a class="popup" rel="prettyPhoto[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/pymDvW2Mvco?iframe=true&width=800&height=600">(Unofficial Video)</a></td>
+        			<td><a rel="geekcampsg[slides]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://www.slideshare.net/slideshow/embed_code/14006860?iframe=true&width=800&height=600" >Learning iOS and hunting NSZombies in 3 weeks</a> by <strong>Calvin Cheng</strong> <a rel="geekcampsg[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/Wh535qOUWw4?iframe=true&width=800&height=600">(Video)</a></td>
+        			<td><a rel="geekcampsg[slides]" title="Loading presentation from external site. Please give it a few seconds to load." href="https://docs.google.com/presentation/embed?id=1mIHAruC3nbI-CZPVjX7AuRp-bj9xjJima0l6XrHUI6Y&start=false&loop=false&delayms=3000?iframe=true&width=800&height=600">Why Common Lisp is still relevant. Introduction to the language</a> by <strong>Elias Mårtenson</strong> <a rel="geekcampsg[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/pymDvW2Mvco?iframe=true&width=800&height=600">(Unofficial Video)</a></td>
         		</tr>
         		<tr>
         			<td>3:45 - 4:30</td>
-        			<td><a class="popup" rel="prettyPhoto[popups]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/502f20ea21ffdc000201a5a7?iframe=true&width=800&height=600">Continuous Integration for PHP with Jenkins &amp Sonar</a> by <strong>Michael Cheng</strong> <a class="popup" rel="prettyPhoto[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/fvIxw70jlZ8?iframe=true&width=800&height=600">(Video)</a></td>
-                    <td><a class="popup" rel="prettyPhoto[popups]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/503043fbedfca80002010b93?iframe=true&width=800&height=600">Designing a RESTful API server for a mobile app</a> by <strong>Mugunth</strong></td>
+        			<td><a rel="geekcampsg[slides]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/502f20ea21ffdc000201a5a7?iframe=true&width=800&height=600">Continuous Integration for PHP with Jenkins &amp Sonar</a> by <strong>Michael Cheng</strong> <a rel="geekcampsg[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/fvIxw70jlZ8?iframe=true&width=800&height=600">(Video)</a></td>
+                    <td><a rel="geekcampsg[slides]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/503043fbedfca80002010b93?iframe=true&width=800&height=600">Designing a RESTful API server for a mobile app</a> by <strong>Mugunth</strong></td>
         		</tr>
         		<tr>
         			<td>4:30 - 5:15</td>
-        			<td><a class="popup" rel="prettyPhoto[popups]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/5030344a9c9652000200ea4b?iframe=true&width=800&height=600">How to stay up when your server goes down (that includes AWS too)</a> by <strong>Laurence Putra</strong> <a class="popup" rel="prettyPhoto[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/YxUK_S5Ydhc?iframe=true&width=800&height=600">(Video)</a></td>
-        			<td><a class="popup" rel="prettyPhoto[popups]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://prezi.com/8mgqtxcc0wzu/view?iframe=true&width=800&height=600" >FPGAs - Embedded Protoyping Made Easier</a> by <strong>Aashish Mehta</strong></td>
+        			<td><a rel="geekcampsg[slides]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/5030344a9c9652000200ea4b?iframe=true&width=800&height=600">How to stay up when your server goes down (that includes AWS too)</a> by <strong>Laurence Putra</strong> <a rel="geekcampsg[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/YxUK_S5Ydhc?iframe=true&width=800&height=600">(Video)</a></td>
+        			<td><a rel="geekcampsg[slides]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://prezi.com/8mgqtxcc0wzu/view?iframe=true&width=800&height=600" >FPGAs - Embedded Protoyping Made Easier</a> by <strong>Aashish Mehta</strong></td>
         		</tr>
         		<tr>
         			<td>5:15 - 6:00</td>
-        			<td><a class="popup" rel="prettyPhoto[popups]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/50335865abb2600002030d58?iframe=true&width=800&height=600">Graph databases for fun and profit</a> by <strong>Vivek Prahlad</strong> <a class="popup" rel="prettyPhoto[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/MnDk56lLcuw?iframe=true&width=800&height=600">(Video)</a></td>
+        			<td><a rel="geekcampsg[slides]" title="Loading presentation from external site. Please give it a few seconds to load." href="http://speakerdeck.com/embed/50335865abb2600002030d58?iframe=true&width=800&height=600">Graph databases for fun and profit</a> by <strong>Vivek Prahlad</strong> <a rel="geekcampsg[videos]" title="Loading video from external site. Please give it a few seconds to load." href="http://www.youtube.com/embed/MnDk56lLcuw?iframe=true&width=800&height=600">(Video)</a></td>
                     <td>Youngling Special! <p>Listen to some of the cool things our younglings in school are hacking around with these days.</p></td>
         		</tr>
         		<tr>
@@ -77,11 +77,11 @@
 <script type="text/javascript" src="<?php echo base_url('static/js/jquery.prettyPhoto.js')?>"></script>
 <script type="text/javascript">
   $(document).ready(function(){
-    $("a[rel='prettyPhoto[popups]']").prettyPhoto({
+    $("a[rel='geekcampsg[slides]']").prettyPhoto({
         social_tools: false,
         allow_expand: false,
     });
-    $("a[rel='prettyPhoto[videos]']").prettyPhoto({
+    $("a[rel='geekcampsg[videos]']").prettyPhoto({
         social_tools: false,
         allow_expand: false,
     });
