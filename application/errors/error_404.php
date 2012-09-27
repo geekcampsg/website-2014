1fec5f54f6065a91a1f1b3e34c8618950c1e9022
laurenceputra <laurenceputra@gmail.com>
2012-09-27 12:45:50 +0800

    added notfound

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -38,7 +38,7 @@
         <div class="span12">
   		    <div class="padded-element">
   		        <h2 class="about">Oops! A page is lost!</h2>
-  		        <p class="info">If you are very sure this page exists, drop us an email <a href="<?php echo site_url('contact') ?>">here</a>.</p>
+  		        <p class="info">If you are very sure this page exists, drop us an email <a href="http://www.geekcamp.sg/pages/email">here</a>.</p>
   		        <p class="info">Else you can head over to <a href="./">./</a> and submit a talk, or vote for your favourite talks :)</p>
   		    </div>
 		    </div> 
