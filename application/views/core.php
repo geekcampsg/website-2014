ab473732d39e3f480942290a6fdeb0cc08a41417
laurenceputra <laurenceputra@gmail.com>
2012-09-30 00:06:39 +0800

    updated

--- application/views/core.php
+++ application/views/core.php
@@ -47,11 +47,11 @@ mixpanel.init("adeb4cfee8bc300c7ab493ca2052c1b5");</script><!-- end Mixpanel -->
       <hr>     
       <footer>
         <div class="row">
-        <div class="span4">
-          <p>Bring a youngling to GeekcampSG this year!</p>
-          <p style="display:none">Loading took {elapsed_time}s</p>
+          <div class="span4">
+            <p>Bring a youngling to GeekcampSG this year!</p>
+            <p style="display:none">Loading took {elapsed_time}s.</p>
+          </div>
           <div style="display:none"><a href="https://mixpanel.com/f/partner"><img src="//cdn.mxpnl.com/site_media/images/partner/badge_blue.png" alt="Mobile Analytics" /></a></div>
-        </div>
         </div>
       </footer>
     </div> <!-- /container -->
