a3d4657790829d0f3f6faa2a47a34de5268ba30f
laurenceputra <laurenceputra@gmail.com>
2013-03-25 13:42:27 +0800

    updated talk page

--- application/views/talk/view.php
+++ application/views/talk/view.php
@@ -1,7 +1,5 @@
 <div class="span12">
     <div class="padded-element">
-        <h3>View more talks  <a href="<?php echo base_url()?>#talks">here</a>!</h3>
-        <p class="subsubtitle">Click on the Facebook Like Button, or Google +1 button to vote.</p>
         <h2><?php echo $talk['title']?></h2>
         <h3>By <?php echo $talk['speaker_name'] ?></h3>
         <p class="subtitle">
@@ -19,6 +17,8 @@
         <br />
         <div style="width:90px;" class="fb-like" data-href="<?php echo $url_share_normal ?>" data-send="false" data-layout="button_count" data-width="90" data-show-faces="false"></div>
         <div class="g-plusone" data-href="<?php echo $url_share_normal ?>" data-width="90" data-size="medium"></div>
+        <h3>View more talks  <a href="<?php echo base_url()?>#talks">here</a>!</h3>
+        <p class="subsubtitle">Click on the Facebook 'Like' button or Google +1 button to vote.</p>
     </div>
 </div>
 <script type="text/javascript">
