eebe91090587b47ab833dc0db0cea8be32a7ab35
laurenceputra <laurenceputra@gmail.com>
2013-04-06 22:09:49 +0800

    added signup link

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -5,8 +5,8 @@
   <p class="subtitle">Conference for the geeks.</p>
   <p class="subtitle">No startup talks. No how to get rich talks. This is purely a tech conference.</p> 
   <p class="subtitle">Hashtag: <b>#geekcampsg</b> | Date: <b>7th September 2013</b> | Time: <b>9:30am to 6pm (Afterparty afterwards)</b> | Location: <b>Microsoft, 21st Floor</b></p>
+  <p class="subtitle">Sign up over at <a href="http://geekcampsg13.eventbrite.com">Eventbrite</a></p>
   <p class="subtitle">Last year's slides can be found over <a href="<?php echo site_url('schedule/view_schedule/2012') ?>">here</a>.</p>
-  <p class="subtitle">We will be opening registration for both GeekcampSG and the talks soon</p>
   <h2 class="about">Theme</h2>
   <p class="subtitle">This year, our theme is <code>Because we can!</code>. See inspiration <a rel="geekcampsg[video]" onclick="mixpanel.track('Video | Because we can')" title="Loading video from external site. Please give it a few seconds to load." href="https://www.youtube-nocookie.com/embed/HsbvLd7MSWQ?iframe=true&width=800&height=600">here</a>!</p>
   <h2 class="about">Topics up for voting!</h2>
