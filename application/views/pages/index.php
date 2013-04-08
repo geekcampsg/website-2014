ebd8c5bc39fcbfb7249af0b89481d84d9958c30b
laurenceputra <laurenceputra@gmail.com>
2013-04-08 12:32:50 +0800

    added rahul's link

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -58,7 +58,7 @@
     <li>Justin Lee</li>
     <li><a href="http://geeksphere.net" target="_blank">Laurence Putra</a></li>
     <li>Luther Goh</li>
-    <li>Rahul Gokul</li>
+    <li><a href="https://ra.hul.ag/" target="_blank">Rahul Gokul</a></li>
     <li><a href="https://twitter.com/shannietron" target="_blank">Shanmugam Mpl</a></li>
     <li>Sneha Menon</li>
     <li>Vishnu Prem</li>
