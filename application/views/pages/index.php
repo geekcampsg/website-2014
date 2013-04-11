2474b7d92e254f8237d86569538ca251ac411ecf
laurenceputra <laurenceputra@gmail.com>
2013-04-11 14:47:49 +0800

    added crazy egg tracking

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -104,3 +104,9 @@
       allow_expand: false,
   });
 </script>
+<script type="text/javascript">
+setTimeout(function(){var a=document.createElement("script");
+var b=document.getElementsByTagName("script")[0];
+a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0016/1341.js?"+Math.floor(new Date().getTime()/3600000);
+a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+</script>
