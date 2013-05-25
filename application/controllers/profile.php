a516f960757b231fb2c0ffb04bdc6ff4c87564f3
laurenceputra <laurenceputra@gmail.com>
2013-05-25 21:08:56 +0800

    fixed benchmarks calls

--- application/controllers/profile.php
+++ application/controllers/profile.php
@@ -5,20 +5,23 @@ class Profile extends CI_Controller {
 		if(!$this->user_lib->is_logged_in()){
 			redirect('');
 		}
-		$this->benchmark->mark('Code Starts');
+		$this->benchmark->mark('Code Starts_start');
 		$this->load->model('talk_model');
-		$this->benchmark->mark('Model Loaded');
+		$this->benchmark->mark('Code Starts_end');
+		$this->benchmark->mark('Model Loaded_start');
 		$data['talks'] = $this->talk_model->get_all_published_talks_for_year(2013);
-		$this->benchmark->mark('Talks gotten');
+		$this->benchmark->mark('Model Loaded_end');
+		$this->benchmark->mark('Talks gotten_start');
 		$data['content'] = $this->load->view('pages/index', $data, TRUE);
-		$this->benchmark->mark('Page generated');
+		$this->benchmark->mark('Talks gotten_end');
+		$this->benchmark->mark('Page generated_start');
 		if($year > 2000){
 			$this->load->view('core', $data);
 		}
 		else{
 			$this->load->view('core_troll', $data);
 		}
-		$this->benchmark->mark('Core loaded');
+		$this->benchmark->mark('Page generated_end');
 		
 		$this->output->enable_profiler(TRUE);
 	}
@@ -28,21 +31,24 @@ class Profile extends CI_Controller {
 			redirect('');
 		}
 		else{
-			$this->benchmark->mark('Code Starts');
+			$this->benchmark->mark('Code Starts_start');
 			$this->load->model('talk_model');
-			$this->benchmark->mark('Model Loaded');
+			$this->benchmark->mark('Code Starts_end');
+			$this->benchmark->mark('Model Loaded_start');
 			$data['talk'] = $this->talk_model->get_talk_by__id($id);
-			$this->benchmark->mark('Talks gotten');
+			$this->benchmark->mark('Model Loaded_end');
 			if($data['talk'] == NULL){
 				redirect('');
 			}
 			else{
+				$this->benchmark->mark('Talks gotten_start');
 				$data['content'] = $this->load->view('talk/view', $data, TRUE);
-				$this->benchmark->mark('Page generated');
+				$this->benchmark->mark('Talks gotten_end');
+				$this->benchmark->mark('Page generated_start');
 				$data['meta_title'] = $data['talk']['title'].' by '.$data['talk']['speaker_name'];
 				$data['meta_description'] = 'Join me and vote for a talk at Geekcamp.SG today!';
 				$this->load->view('core', $data);
-				$this->benchmark->mark('Core loaded');
+				$this->benchmark->mark('Page generated_end');
 			}
 		}
 		$this->output->enable_profiler(TRUE);
