8de0923e6e1827a2ba7cf25f39e851b879f5905e
laurenceputra <laurenceputra@gmail.com>
2013-05-31 16:54:11 +0800

    finished hotel booking

--- application/views/pages/hotel.php
+++ application/views/pages/hotel.php
@@ -1,1 +1,7 @@
-<iframe width="640" height="360" src="http://tripevent.co/map/embed/geekcampsg/" frameborder="0"></iframe>
+<div class="span12">
+    <div class="padded-element">
+        <h2 class="about">Coming from overseas?</h2>
+        <p class="subtitle">Book a hotel near GeekcampSG from the map below. Simply click on any of the recommended hotels, and your booking process will start.</p>
+        <iframe width="640" height="360" src="http://tripevent.co/map/embed/geekcampsg/" frameborder="0"></iframe>
+    </div>
+</div> 
