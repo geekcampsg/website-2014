43d3bcb9f35b6fc0393a7e64f50808870c08e6c4
laurenceputra <laurenceputra@gmail.com>
2014-05-13 21:08:43 +0800

    initial team page, missing shan and shruti's photos

--- application/views/pages/team.php
+++ application/views/pages/team.php
@@ -1,28 +1,81 @@
 <div class="span12">
 	<div class="padded-element">
 		<h2>The team</h2>
-		<h3>Advisors</h3>
-		Justin Lee <br>
-		Luther Goh <br>
 
-		<h3>Sponsorship</h3>
-		Laurence Putra <br>
-		Vishnu Prem <br>
-		
-		<h3>Hack Booths</h3>
-		Shanmugam Mpl <br>
-		Rahul Gokul <br>
+		<div class="span10">
+			<h3>Emcee</h3>
+			<div class="span3">
+				<img class="teamMemPhoto" src="<?php echo base_url('static/images/team/sayanee.jpg')?>">
+				<h4>Sayanee Basu</h4>
+			</div>
+			<div class="span3">
+				<img class="teamMemPhoto" src="<?php echo base_url('static/images/team/placeholder.jpg')?>">
+				<h4>Shruti Suresh</h4>
+			</div>
+		</div>
 
-		<h3>Video Team</h3>
-		Chinmay <br>
-		Daniel Tsou <br>
-		
-		<h3>Venue</h3>
-		Aizat Omar <br>
+		<div class="span10">
+			<h3>Hack Booths</h3>
+			<div class="span3">
+				<img class="teamMemPhoto" src="<?php echo base_url('static/images/team/rahul.jpg')?>">
+				<h4>Rahul Gokul</h4>
+			</div>
+			<div class="span3">
+				<img class="teamMemPhoto" src="<?php echo base_url('static/images/team/placeholder.jpg')?>">
+				<h4>Shanmugam Mpl</h4>
+			</div>
+		</div>
 
-		<h3>Emcee</h3>
-		Sayanee <br>
-		Shruti <br>
+		<div class="span10">
+			<h3>Operations</h3>
+			<div class="span3">
+				<img class="teamMemPhoto" src="<?php echo base_url('static/images/team/vishnu.jpg')?>">
+				<h4>Vishnu Prem</h4>
+			</div>
+		</div>
 
+		<div class="span10">
+			<h3>Sponsorship</h3>
+			<div class="span3">
+				<img class="teamMemPhoto" src="<?php echo base_url('static/images/team/laurence.jpg')?>">
+				<h4>Laurence Putra</h4>
+			</div>
+		</div>
+
+		<div class="span10">
+			<h3>Venue</h3>
+			<div class="span3">
+				<img class="teamMemPhoto" src="<?php echo base_url('static/images/team/aizat.jpg')?>">
+				<h4>Aizat Omar</h4>
+			</div>
+			<div class="span3">
+				<img class="teamMemPhoto" src="<?php echo base_url('static/images/team/justin.jpg')?>">
+				<h4>Justin Lee</h4>
+			</div>
+			<div class="span3">
+				<img class="teamMemPhoto" src="<?php echo base_url('static/images/team/luther.jpg')?>">
+				<h4>Luther Goh</h4>
+			</div>
+		</div>
+
+		<div class="span10">
+			<h3>Video Team</h3>
+			<div class="span3">
+				<img class="teamMemPhoto" src="<?php echo base_url('static/images/team/chinmay.jpg')?>">
+				<h4>Chinmay</h4>
+			</div>
+			<div class="span3">
+				<img class="teamMemPhoto" src="<?php echo base_url('static/images/team/daniel.jpg')?>">
+				<h4>Daniel Tsou</h4>
+			</div>
+			<div class="span3">
+				<img class="teamMemPhoto" src="<?php echo base_url('static/images/team/mike.jpg')?>">
+				<h4>Michael Cheng</h4>
+			</div>
+		</div>
 	</div>
 </div>
+
+<script type="text/javascript">
+	mixpanel.track("Team Loaded");
+</script>
